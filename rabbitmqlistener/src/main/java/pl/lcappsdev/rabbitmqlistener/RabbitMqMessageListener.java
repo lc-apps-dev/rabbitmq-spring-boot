bd022f56a7a1bc72f48aaa9ac9119e8ee8a03bdb
lc-apps-dev <lc-apps-dev>
2021-11-10 20:56:12 +0100

    added rabbitmq test projects - listener and producer

---
+++ rabbitmqlistener/src/main/java/pl/lcappsdev/rabbitmqlistener/RabbitMqMessageListener.java
@@ -0,0 +1,14 @@
+package pl.lcappsdev.rabbitmqlistener;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+public class RabbitMqMessageListener implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		
+		System.out.println("message = [" + new String(message.getBody()) + "]");
+	}
+
+}
---
+++ rabbitmqlistener/src/main/java/pl/lcappsdev/rabbitmqlistener/RabbitMqQueueConfiguration.java
@@ -0,0 +1,8 @@
+package pl.lcappsdev.rabbitmqlistener;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqQueueConfiguration {
+
+}
